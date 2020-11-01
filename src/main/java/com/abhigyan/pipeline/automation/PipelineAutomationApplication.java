cedc65184a4afa7d482dc38379a7ab9978e84194
Abhigyan Mandal <abhigyanmandal2@gmail.com>
2020-11-01 19:23:05 +0530

    Sample project for Github
    Create new project

---
+++ src/main/java/com/abhigyan/pipeline/automation/PipelineAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.abhigyan.pipeline.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhigyan/pipeline/automation/PipelineAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhigyan.pipeline.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
